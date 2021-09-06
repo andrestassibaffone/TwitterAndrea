3e9c5f59e2b9ba96988b021cffb6d0c5331a251d
andrestassibaffone <90176236+andrestassibaffone@users.noreply.github.com>
2021-09-06 09:09:56 +0200

    Add files via upload

---
+++ esperimento.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          Ciao {this.props.name}
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <HelloMessage name="Claudia" />,
+    document.getElementById('hello-example')
+  );
